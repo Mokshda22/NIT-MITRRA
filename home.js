112a1b936f9a75831f164e3b7e2bc5fedb591529
Mokshda22 <bhattmokshda4@gmail.com>
2026-08-18 01:50:20 +0530

    Fix hero slider buttons markup and carousel controller

--- home.js
+++ home.js
@@ -1,49 +1,69 @@
-//step 1: get DOM
-let nextDom = document.getElementById('next');
-let prevDom = document.getElementById('prev');
-
-let carouselDom = document.querySelector('.carousel');
-let SliderDom = carouselDom.querySelector('.carousel .list');
-let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
-let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
-let timeDom = document.querySelector('.carousel .time');
-
-thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 3000;
-let timeAutoNext = 7000;
-
-nextDom.onclick = function(){
-    showSlider('next');    
-}
-
-prevDom.onclick = function(){
-    showSlider('prev');    
-}
-let runTimeOut;
-let runNextAuto = setTimeout(() => {
-    next.click();
-}, timeAutoNext)
-function showSlider(type){
-    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
-    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+// NIT-MITRRA Hero Carousel Controller
+document.addEventListener('DOMContentLoaded', () => {
+    const nextDom = document.getElementById('next');
+    const prevDom = document.getElementById('prev');
+    const carouselDom = document.querySelector('.carousel');
     
-    if(type === 'next'){
-        SliderDom.appendChild(SliderItemsDom[0]);
-        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-        carouselDom.classList.add('next');
-    }else{
-        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-        carouselDom.classList.add('prev');
-    }
-    clearTimeout(runTimeOut);
-    runTimeOut = setTimeout(() => {
-        carouselDom.classList.remove('next');
-        carouselDom.classList.remove('prev');
-    }, timeRunning);
+    if (!carouselDom) return;
 
-    clearTimeout(runNextAuto);
-    runNextAuto = setTimeout(() => {
-        next.click();
-    }, timeAutoNext)
-}
+    const SliderDom = carouselDom.querySelector('.list');
+    const thumbnailBorderDom = carouselDom.querySelector('.thumbnail');
+    
+    if (!SliderDom || !thumbnailBorderDom) return;
+
+    const thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+    if (thumbnailItemsDom.length > 0) {
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+    }
+
+    const timeRunning = 3000;
+    const timeAutoNext = 7000;
+    let runTimeOut;
+    let runNextAuto;
+
+    function resetAutoTimer() {
+        clearTimeout(runNextAuto);
+        runNextAuto = setTimeout(() => {
+            if (nextDom) showSlider('next');
+        }, timeAutoNext);
+    }
+
+    function showSlider(type) {
+        const SliderItemsDom = SliderDom.querySelectorAll('.item');
+        const currentThumbnails = thumbnailBorderDom.querySelectorAll('.item');
+        
+        if (SliderItemsDom.length === 0 || currentThumbnails.length === 0) return;
+
+        if (type === 'next') {
+            SliderDom.appendChild(SliderItemsDom[0]);
+            thumbnailBorderDom.appendChild(currentThumbnails[0]);
+            carouselDom.classList.add('next');
+        } else {
+            SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+            thumbnailBorderDom.prepend(currentThumbnails[currentThumbnails.length - 1]);
+            carouselDom.classList.add('prev');
+        }
+
+        clearTimeout(runTimeOut);
+        runTimeOut = setTimeout(() => {
+            carouselDom.classList.remove('next');
+            carouselDom.classList.remove('prev');
+        }, timeRunning);
+
+        resetAutoTimer();
+    }
+
+    if (nextDom) {
+        nextDom.addEventListener('click', () => {
+            showSlider('next');
+        });
+    }
+
+    if (prevDom) {
+        prevDom.addEventListener('click', () => {
+            showSlider('prev');
+        });
+    }
+
+    resetAutoTimer();
+});
